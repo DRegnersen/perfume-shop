3433e7558ef29814ebe281e3cefabc40941b09d2
d.dubovikov <d.dubovikov@tinkoff.ru>
2023-11-17 15:07:50 +0300

    optimize classes

--- scripts/selectNavBarItem.js
+++ scripts/selectNavBarItem.js
@@ -1,13 +1,13 @@
 function selectNavBarItem() {
     const currentPage = window.location.href.split('?')[0];
 
-    const navBarItems = document.querySelectorAll('.nav-bar__item a');
+    const navBarItems = document.querySelectorAll('.nav-bar__item');
 
     navBarItems.forEach(navBarItem => {
         if (navBarItem.href === currentPage) {
-            navBarItem.classList.add('nav-bar__item-text__active');
+            navBarItem.classList.add('nav-bar__item__active');
         } else {
-            navBarItem.classList.remove('nav-bar__item-text__active');
+            navBarItem.classList.remove('nav-bar__item__active');
         }
     });
 }
